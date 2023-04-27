1f512676b6c51d6e76d366f9b203f34b706669e9
woodsdeshe <woodsdeshe94@gmail.com>
2023-04-27 16:03:26 -0400

    first commit with dependencies added

---
+++ Spring-Mini-Project/src/test/java/com/miniproject/Spring/Mini/Project/SpringMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniproject.Spring.Mini.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMiniProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
