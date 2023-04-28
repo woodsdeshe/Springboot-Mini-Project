acdaba719bf35d3182af62992b8f9cd19be4a921
woodsdeshe <woodsdeshe94@gmail.com>
2023-04-28 11:59:02 -0400

    Added hairCareService variable to HairCareController in order to access getHairCategories() method

--- Spring-Mini-Project/src/main/java/com/miniproject/Spring/Mini/Project/controller/HairCareController.java
+++ Spring-Mini-Project/src/main/java/com/miniproject/Spring/Mini/Project/controller/HairCareController.java
@@ -1,14 +1,26 @@
 package com.miniproject.Spring.Mini.Project.controller;
 
 import com.miniproject.Spring.Mini.Project.model.HairCareCategory;
+import com.miniproject.Spring.Mini.Project.service.HairCareService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api")
 public class HairCareController {
 
-    @GetMapping(path = "/categories")
-    public HairCareCategory
+    private HairCareService hairCareService;
+
+    public void setHairCareService(HairCareService hairCareService) {
+        this.hairCareService = hairCareService;
+    }
+
+    // http://localhost:9093/api/categories/
+    @GetMapping(path = "/categories/")
+    public List<HairCareCategory> getCategories() {
+
+    }
 }
