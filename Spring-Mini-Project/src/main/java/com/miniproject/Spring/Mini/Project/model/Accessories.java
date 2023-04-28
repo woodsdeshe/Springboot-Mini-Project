e4eed24eaea877d3d05db58871d356e170fbde0b
woodsdeshe <woodsdeshe94@gmail.com>
2023-04-28 18:20:28 -0400

    Completed Accessories model

--- Spring-Mini-Project/src/main/java/com/miniproject/Spring/Mini/Project/model/Accessories.java
+++ Spring-Mini-Project/src/main/java/com/miniproject/Spring/Mini/Project/model/Accessories.java
@@ -1,10 +1,73 @@
 package com.miniproject.Spring.Mini.Project.model;
 
-@E
+import javax.persistence.*;
+
+@Entity
+@Table(name = "accessories")
 public class Accessories {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
     private String name;
+
+    @Column
     private String description;
+
+    @Column
     private Integer rating;
 
+    public Accessories() {
+    }
+
+    public Accessories(Long id, String name, String description, Integer rating) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.rating = rating;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return "Accessories{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", rating=" + rating +
+                '}';
+    }
 }
