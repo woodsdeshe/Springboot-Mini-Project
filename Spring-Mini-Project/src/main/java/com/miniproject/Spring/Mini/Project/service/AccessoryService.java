21109dee21c73d77ad8ae00ab21fc1d3af667ef9
woodsdeshe <woodsdeshe94@gmail.com>
2023-04-28 18:47:16 -0400

    Created AccessoryService

---
+++ Spring-Mini-Project/src/main/java/com/miniproject/Spring/Mini/Project/service/AccessoryService.java
@@ -0,0 +1,10 @@
+package com.miniproject.Spring.Mini.Project.service;
+
+import com.miniproject.Spring.Mini.Project.repository.AccessoriesRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccessoryService {
+
+    private AccessoriesRepository accessoriesRepository;
+}
