d5f0a4421d0c0c8588e0c40a1de070c8964ecd6f
woodsdeshe <woodsdeshe94@gmail.com>
2023-04-28 18:18:18 -0400

    Created Accessories model

---
+++ Spring-Mini-Project/src/main/java/com/miniproject/Spring/Mini/Project/model/Accessories.java
@@ -0,0 +1,10 @@
+package com.miniproject.Spring.Mini.Project.model;
+
+@E
+public class Accessories {
+    private Long id;
+    private String name;
+    private String description;
+    private Integer rating;
+
+}
