1682bcec67bc38769a51b2cf5ea1f7501fdf332e
woodsdeshe <woodsdeshe94@gmail.com>
2023-04-28 18:20:28 -0400

    Completed Accessories model

---
+++ Spring-Mini-Project/src/main/java/com/miniproject/Spring/Mini/Project/repository/AccessroiesRepository.java
@@ -0,0 +1,8 @@
+package com.miniproject.Spring.Mini.Project.repository;
+
+import com.miniproject.Spring.Mini.Project.model.Accessories;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccessoriesRepository extends JpaRepository<Accessories, Long> {
+
+}
