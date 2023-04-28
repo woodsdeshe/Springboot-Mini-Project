3c1e2e080c9284d688dd3af99e143861dcf6239c
woodsdeshe <woodsdeshe94@gmail.com>
2023-04-28 11:21:56 -0400

    Added @Service annotation to HairCareService and autowired the HairCareRepository object

--- Spring-Mini-Project/src/main/java/com/miniproject/Spring/Mini/Project/service/HairCareService.java
+++ Spring-Mini-Project/src/main/java/com/miniproject/Spring/Mini/Project/service/HairCareService.java
@@ -1,4 +1,18 @@
 package com.miniproject.Spring.Mini.Project.service;
 
+import com.miniproject.Spring.Mini.Project.repository.HairCareRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class HairCareService {
+
+    private HairCareRepository hairCareRepository;
+
+    @Autowired
+    public void setHairCareRepository(HairCareRepository hairCareRepository) {
+        this.hairCareRepository = hairCareRepository;
+    }
+
+    public
 }
