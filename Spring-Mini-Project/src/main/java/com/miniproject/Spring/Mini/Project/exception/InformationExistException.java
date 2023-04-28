ffea3e60a4b2e120910a5b275ac644cdccf509ee
woodsdeshe <woodsdeshe94@gmail.com>
2023-04-28 12:23:22 -0400

    Comments of InformationExistException created

--- Spring-Mini-Project/src/main/java/com/miniproject/Spring/Mini/Project/exception/InformationExistException.java
+++ Spring-Mini-Project/src/main/java/com/miniproject/Spring/Mini/Project/exception/InformationExistException.java
@@ -3,6 +3,10 @@ package com.miniproject.Spring.Mini.Project.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * The response annotation lets us know that the exception will result in a http response of conflict.
+ * The conflict value lets us know that the users request can't be carried out because there is a conflict with the CRUD operations of the API
+ */
 @ResponseStatus(HttpStatus.CONFLICT)
 public class InformationExistException extends RuntimeException{
     public InformationExistException(String message) {
