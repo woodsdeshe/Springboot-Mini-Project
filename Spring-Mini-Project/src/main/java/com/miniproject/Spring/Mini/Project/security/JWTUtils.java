da36465b9de5457d7c928bcd7627ea9108e6966a
woodsdeshe <woodsdeshe94@gmail.com>
2023-04-30 11:50:17 -0400

    Updated JWTUtils

--- Spring-Mini-Project/src/main/java/com/miniproject/Spring/Mini/Project/security/JWTUtils.java
+++ Spring-Mini-Project/src/main/java/com/miniproject/Spring/Mini/Project/security/JWTUtils.java
@@ -1,9 +1,11 @@
 package com.miniproject.Spring.Mini.Project.security;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.logging.Logger;
 
 @Service
@@ -19,6 +21,13 @@ public class JWTUtils {
     public String generateJwtToken(MyUserDetails myUserDetails) {
         return Jwts.builder()
                 .setSubject((myUserDetails.getUsername()))
-                .se
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)
+                .compact();
+    }
+
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(jwtSecret).build().parseClaimsJwt(token).getBody().getSubject();
     }
 }
