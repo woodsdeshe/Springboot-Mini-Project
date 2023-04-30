a07bc430ee5f7a90ccfce6da05e353fec546cb78
woodsdeshe <woodsdeshe94@gmail.com>
2023-04-30 11:00:12 -0400

    Added dependency and started createUser method in UserService

--- Spring-Mini-Project/src/main/java/com/miniproject/Spring/Mini/Project/service/UserService.java
+++ Spring-Mini-Project/src/main/java/com/miniproject/Spring/Mini/Project/service/UserService.java
@@ -2,17 +2,32 @@ package com.miniproject.Spring.Mini.Project.service;
 
 import com.miniproject.Spring.Mini.Project.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import com.miniproject.Spring.Mini.Project.model.User;
 
 @Service
 public class UserService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public void setUserRepository(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
+    public User createUser(User userObject) {
+        System.out.println("service calling createUser ==>");
+        if (!userRepository.existsByEmailAddress(userObject.getEmailAddress())) {
+            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
+            return userRepository.save(userObject);
+        } else {
+            throw
+
+        }
+    }
 
 }
