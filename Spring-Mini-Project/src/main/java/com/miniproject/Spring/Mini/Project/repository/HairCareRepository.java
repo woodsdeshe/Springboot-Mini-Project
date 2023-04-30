ff33618f337abd04b65166d391e92be4535b553e
woodsdeshe <woodsdeshe94@gmail.com>
2023-04-30 12:14:47 -0400

    Updated HairCareRepository

--- Spring-Mini-Project/src/main/java/com/miniproject/Spring/Mini/Project/repository/HairCareRepository.java
+++ Spring-Mini-Project/src/main/java/com/miniproject/Spring/Mini/Project/repository/HairCareRepository.java
@@ -3,9 +3,17 @@ package com.miniproject.Spring.Mini.Project.repository;
 import com.miniproject.Spring.Mini.Project.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HairCareRepository extends JpaRepository<Category, Long> {
     Category findByName(String name);
 
     Category findHairCareCategoryById(Long hairCategoryId);
+
+    Category findByIdAndUserId(Long hairCategoryId, Long userId);
+
+    List<Category> findByUserId(Long userId);
+
+    Category findByUserIdAndAndName(Long userId, String categoryName);
 }
 
