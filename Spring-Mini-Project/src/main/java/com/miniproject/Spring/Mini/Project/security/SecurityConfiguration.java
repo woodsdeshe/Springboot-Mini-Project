1fd0794626eff3d4ca20a406460f706515510f31
woodsdeshe <woodsdeshe94@gmail.com>
2023-04-30 11:28:26 -0400

    Added JWT dependencies

--- Spring-Mini-Project/src/main/java/com/miniproject/Spring/Mini/Project/security/SecurityConfiguration.java
+++ Spring-Mini-Project/src/main/java/com/miniproject/Spring/Mini/Project/security/SecurityConfiguration.java
@@ -1,4 +1,19 @@
 package com.miniproject.Spring.Mini.Project.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
+
+    private MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    public void setMyUserDetailsService(MyUserDetailsService myUserDetailsService) {
+        this.myUserDetailsService = myUserDetailsService;
+    }
+
+
 }
