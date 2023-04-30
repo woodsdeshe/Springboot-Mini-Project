0694e116412ed4f0e5feb31d85c1135f57eb65a8
woodsdeshe <woodsdeshe94@gmail.com>
2023-04-30 11:41:28 -0400

    Updated path in UserController to create user

--- Spring-Mini-Project/src/main/java/com/miniproject/Spring/Mini/Project/controller/UserController.java
+++ Spring-Mini-Project/src/main/java/com/miniproject/Spring/Mini/Project/controller/UserController.java
@@ -1,9 +1,27 @@
 package com.miniproject.Spring.Mini.Project.controller;
 
+import com.miniproject.Spring.Mini.Project.model.User;
+import com.miniproject.Spring.Mini.Project.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/api")
 public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public User createUser(@RequestBody User userObject) {
+        System.out.println("service calling createUser ==>");
+        return userService.createUser(userObject);
+    }
 }
