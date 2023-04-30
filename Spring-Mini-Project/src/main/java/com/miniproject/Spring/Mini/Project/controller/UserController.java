be83b2e247ea2c36638b6873af1d395ca25cda0f
woodsdeshe <woodsdeshe94@gmail.com>
2023-04-30 11:02:18 -0400

    Created UserController

---
+++ Spring-Mini-Project/src/main/java/com/miniproject/Spring/Mini/Project/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.miniproject.Spring.Mini.Project.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api")
+public class UserController {
+}
