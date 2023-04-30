cadc423f232ea6f0d4fc630555628204e42c857a
woodsdeshe <woodsdeshe94@gmail.com>
2023-04-30 10:50:53 -0400

    Created UserService class

---
+++ Spring-Mini-Project/src/main/java/com/miniproject/Spring/Mini/Project/service/UserService.java
@@ -0,0 +1,18 @@
+package com.miniproject.Spring.Mini.Project.service;
+
+import com.miniproject.Spring.Mini.Project.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+}
