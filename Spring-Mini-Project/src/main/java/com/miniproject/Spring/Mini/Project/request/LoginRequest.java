6e62c07ca4d49c62b48cef0a62d015f72f6df805
woodsdeshe <woodsdeshe94@gmail.com>
2023-04-30 11:37:16 -0400

    Created LoginRequest

---
+++ Spring-Mini-Project/src/main/java/com/miniproject/Spring/Mini/Project/request/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.miniproject.Spring.Mini.Project.request;
+
+public class LoginRequest {
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
